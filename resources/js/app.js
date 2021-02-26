d3e73daa3429e22e9d5708fb98a9da10ffe36f8c
Sergey Harini <harini.ua@gmail.com>
2021-02-26 14:19:29 +0200

    General-T0: Install apexcharts

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,12 +3,16 @@ import VueMeta from 'vue-meta'
 import PortalVue from 'portal-vue'
 import { InertiaApp } from '@inertiajs/inertia-vue'
 import { InertiaProgress } from '@inertiajs/progress/src'
+import VueApexCharts from 'vue-apexcharts'
 
 Vue.config.productionTip = false
 Vue.mixin({ methods: { route: window.route } })
 Vue.use(InertiaApp)
 Vue.use(PortalVue)
 Vue.use(VueMeta)
+
+Vue.use(VueApexCharts)
+Vue.component('apexchart', VueApexCharts)
 
 InertiaProgress.init()
 
