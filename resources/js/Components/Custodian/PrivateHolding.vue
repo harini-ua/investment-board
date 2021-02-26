cd1a26d51be462bf42fcbf132d83b54be7449878
Sergey Harini <harini.ua@gmail.com>
2021-02-26 16:18:13 +0200

    FE-T3: Custodian page - Init chart

---
+++ resources/js/Components/Custodian/PrivateHolding.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <apexchart width="500" type="pie" :options="options" :series="series"></apexchart>
+  </div>
+</template>
+
+<script>
+
+export default {
+  metaInfo: { title: 'Private Holding' },
+  data: function() {
+    return {
+      options: {
+        labels: ['Private Equity', 'Real Estate'],
+        dataLabels: {
+          enabled: false,
+        },
+      },
+      series: [74.6, 25.4],
+    }
+  },
+}
+</script>
--- resources/js/Pages/Custodian/Index.vue
+++ resources/js/Pages/Custodian/Index.vue
@@ -1,14 +1,20 @@
 <template>
   <div>
     <h1 class="mb-8 font-bold text-3xl">Custodian</h1>
+
+    <private-holding />
   </div>
 </template>
 
 <script>
 import Layout from '@/Shared/Layout'
+import PrivateHolding from '@/Components/Custodian/PrivateHolding'
 
 export default {
   metaInfo: { title: 'Custodian' },
   layout: Layout,
+  components: {
+    PrivateHolding
+  },
 }
 </script>
