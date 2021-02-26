d255ce351f8d95f2631bfb6b9b55b0576602edd5
Sergey Harini <harini.ua@gmail.com>
2021-02-26 18:14:47 +0200

    General-T0: Init filter

---
+++ resources/js/Shared/Filter.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Welcome, Mr B. Beyeler</h1>
+    <p>Please pick the date range, base currency, asset class, custodians and account if you need a custom consolidated summary.</p>
+    <slot />
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
