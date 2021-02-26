15b89b09581c935945401c9b5cb7bc4da716ba79
Sergey Harini <harini.ua@gmail.com>
2021-02-26 16:17:08 +0200

    BE-T4: Currency page - Init chart

---
+++ resources/js/Components/Currency/CurrencyExposure.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <apexchart width="500" type="pie" :options="options" :series="series"></apexchart>
+  </div>
+</template>
+
+<script>
+
+export default {
+  metaInfo: { title: 'Currency Exposure' },
+  data: function() {
+    return {
+      options: {
+        labels: ['CHF', 'EUR', 'USD', 'GBP', 'NOK', 'SEK', 'DKK', 'JPY', 'HKD', 'CAD'],
+        dataLabels: {
+          enabled: false,
+        },
+      },
+      series: [3.7, 495, 40.1, 4.1, 2.4, 0.3, 0, 2.4, 2.4],
+    }
+  },
+}
+</script>
--- resources/js/Pages/Currency/Index.vue
+++ resources/js/Pages/Currency/Index.vue
@@ -1,14 +1,20 @@
 <template>
   <div>
     <h1 class="mb-8 font-bold text-3xl">Currency</h1>
+
+    <currency-exposure />
   </div>
 </template>
 
 <script>
 import Layout from '@/Shared/Layout'
+import CurrencyExposure from '@/Components/Currency/CurrencyExposure'
 
 export default {
   metaInfo: { title: 'Currency' },
   layout: Layout,
+  components: {
+    CurrencyExposure
+  },
 }
 </script>
