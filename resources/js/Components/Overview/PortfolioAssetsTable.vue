800ed61a2f13c462861758d4c4b4f0dba32d466a
Sergey Harini <harini.ua@gmail.com>
2021-03-01 18:52:33 +0200

    FE-T2: Overview page / table

--- resources/js/Components/Overview/PortfolioAssetsTable.vue
+++ resources/js/Components/Overview/PortfolioAssetsTable.vue
@@ -2,211 +2,246 @@
   <div class="block-table">
     <h2>Asset Classes</h2>
     <div class="table-holder">
-      <h3 class="table__title">Portfolio assets</h3>
+      <h3 class="table-title">Portfolio assets</h3>
       <div class="table table--portfolio-assets">
         <div class="thead">
           <div class="tr">
-            <div class="td">
-              Asset name
-            </div>
-            <div class="td">
-              Value
-            </div>
-            <div class="td">
-              Month
-            </div>
-            <div class="td">
-              Year
-            </div>
-            <div class="td">
-              Month | Benchmark
-            </div>
-            <div class="td">
-              Year | Benchmark
-            </div>
+            <div class="td">Asset name</div>
+            <div class="td">Value</div>
+            <div class="td hidden-sm">Month</div>
+            <div class="td hidden-sm">Year</div>
+            <div class="td hidden-lg">Month | Benchmark</div>
+            <div class="td hidden-lg">Year | Benchmark</div>
           </div>
         </div>
         <div class="tbody">
-          <a href="#" class="tr">
+          <div class="tr">
             <div class="td">Cash</div>
-            <div class="td"><span class="value">15 957 949</span></div>
-            <div class="td">(200 889)</div>
-            <div class="td">(200 889)</div>
-            <div class="td">
+            <div class="td"><a href="#" class="value">15 957 949</a></div>
+            <div class="td hidden-sm">(200 889)</div>
+            <div class="td hidden-sm">(200 889)</div>
+            <div class="td hidden-lg">
               <div class="double-value-holder">
                 <span class="double-value">-1.26%</span>
                 <span class="double-value">5.21%</span>
               </div>
             </div>
-            <div class="td">
+            <div class="td hidden-lg">
               <div class="double-value-holder">
                 <span class="double-value">-1.26%</span>
                 <span class="double-value">5.21%</span>
               </div>
             </div>
-          </a>
-          <a href="#" class="tr">
+          </div>
+          <div class="collapse-content collapse-content--portfolio-assets">
+            <div class="collapse-content-table">
+              <div class="row visible-lg">
+                <div class="cell"></div>
+                <div class="cell cell-description">Category</div>
+                <div class="cell cell-description">Benchmark</div>
+              </div>
+              <div class="row visible-md">
+                <div class="cell cell-description">P&L MTD</div>
+                <div class="cell">149 811</div>
+                <div class="cell"></div>
+              </div>
+              <div class="row visible-md">
+                <div class="cell cell-description">P&L YTD</div>
+                <div class="cell">(2 013 957)</div>
+                <div class="cell"></div>
+              </div>
+              <div class="row visible-lg">
+                <div class="cell cell-description">% MTD</div>
+                <div class="cell">0.72%</div>
+                <div class="cell">5.21%</div>
+              </div>
+              <div class="row visible-lg">
+                <div class="cell cell-description">% YTD</div>
+                <div class="cell">-5.51%</div>
+                <div class="cell">-1.50%</div>
+              </div>
+            </div>
+          </div>
+          <div class="tr">
+            <div class="td">Equity</div>
+            <div class="td"><a href="#" class="value">15 957 949</a></div>
+            <div class="td hidden-sm">(200 889)</div>
+            <div class="td hidden-sm">(200 889)</div>
+            <div class="td hidden-lg">
+              <div class="double-value-holder">
+                <span class="double-value">-1.26%</span>
+                <span class="double-value">5.21%</span>
+              </div>
+            </div>
+            <div class="td hidden-lg">
+              <div class="double-value-holder">
+                <span class="double-value">-1.26%</span>
+                <span class="double-value">5.21%</span>
+              </div>
+            </div>
+          </div>
+          <div class="tr">
+            <div class="td">KFP Equity </div>
+            <div class="td"><a href="#" class="value">15 957 949</a></div>
+            <div class="td hidden-sm">(200 889)</div>
+            <div class="td hidden-sm">(200 889)</div>
+            <div class="td hidden-lg">
+              <div class="double-value-holder">
+                <span class="double-value">-1.26%</span>
+                <span class="double-value">5.21%</span>
+              </div>
+            </div>
+            <div class="td hidden-lg">
+              <div class="double-value-holder">
+                <span class="double-value">-1.26%</span>
+                <span class="double-value">5.21%</span>
+              </div>
+            </div>
+          </div>
+          <div class="tr">
+            <div class="td">Bond</div>
+            <div class="td"><a href="#" class="value">15 957 949</a></div>
+            <div class="td hidden-sm">(200 889)</div>
+            <div class="td hidden-sm">(200 889)</div>
+            <div class="td hidden-lg">
+              <div class="double-value-holder">
+                <span class="double-value">-1.26%</span>
+                <span class="double-value">5.21%</span>
+              </div>
+            </div>
+            <div class="td hidden-lg">
+              <div class="double-value-holder">
+                <span class="double-value">-1.26%</span>
+                <span class="double-value">5.21%</span>
+              </div>
+            </div>
+          </div>
+          <div class="tr">
+            <div class="td">KFP Bond</div>
+            <div class="td"><a href="#" class="value">15 957 949</a></div>
+            <div class="td hidden-sm">(200 889)</div>
+            <div class="td hidden-sm">(200 889)</div>
+            <div class="td hidden-lg">
+              <div class="double-value-holder">
+                <span class="double-value">-1.26%</span>
+                <span class="double-value">5.21%</span>
+              </div>
+            </div>
+            <div class="td hidden-lg">
+              <div class="double-value-holder">
+                <span class="double-value">-1.26%</span>
+                <span class="double-value">5.21%</span>
+              </div>
+            </div>
+          </div>
+          <div class="tr">
+            <div class="td">Hedge Fund</div>
+            <div class="td"><a href="#" class="value">15 957 949</a></div>
+            <div class="td hidden-sm">(200 889)</div>
+            <div class="td hidden-sm">(200 889)</div>
+            <div class="td hidden-lg">
+              <div class="double-value-holder">
+                <span class="double-value">-1.26%</span>
+                <span class="double-value">5.21%</span>
+              </div>
+            </div>
+            <div class="td hidden-lg">
+              <div class="double-value-holder">
+                <span class="double-value">-1.26%</span>
+                <span class="double-value">5.21%</span>
+              </div>
+            </div>
+          </div>
+          <div class="tr">
+            <div class="td">Commodity</div>
+            <div class="td"><a href="#" class="value">15 957 949</a></div>
+            <div class="td hidden-sm">(200 889)</div>
+            <div class="td hidden-sm">(200 889)</div>
+            <div class="td hidden-lg">
+              <div class="double-value-holder">
+                <span class="double-value">-1.26%</span>
+                <span class="double-value">5.21%</span>
+              </div>
+            </div>
+            <div class="td hidden-lg">
+              <div class="double-value-holder">
+                <span class="double-value">-1.26%</span>
+                <span class="double-value">5.21%</span>
+              </div>
+            </div>
+          </div>
+          <div class="tr">
             <div class="td">Cash</div>
-            <div class="td"><span class="value">15 957 949</span></div>
-            <div class="td">(200 889)</div>
-            <div class="td">(200 889)</div>
-            <div class="td">
+            <div class="td"><a href="#" class="value">15 957 949</a></div>
+            <div class="td hidden-sm">(200 889)</div>
+            <div class="td hidden-sm">(200 889)</div>
+            <div class="td hidden-lg">
               <div class="double-value-holder">
                 <span class="double-value">-1.26%</span>
                 <span class="double-value">5.21%</span>
               </div>
             </div>
-            <div class="td">
+            <div class="td hidden-lg">
               <div class="double-value-holder">
                 <span class="double-value">-1.26%</span>
                 <span class="double-value">5.21%</span>
               </div>
             </div>
-          </a>
-          <a href="#" class="tr">
-            <div class="td">Cash</div>
-            <div class="td"><span class="value">15 957 949</span></div>
-            <div class="td">(200 889)</div>
-            <div class="td">(200 889)</div>
-            <div class="td">
+          </div>
+          <div class="tr">
+            <div class="td">Private Equity</div>
+            <div class="td"><a href="#" class="value">15 957 949</a></div>
+            <div class="td hidden-sm">(200 889)</div>
+            <div class="td hidden-sm">(200 889)</div>
+            <div class="td hidden-lg">
               <div class="double-value-holder">
                 <span class="double-value">-1.26%</span>
                 <span class="double-value">5.21%</span>
               </div>
             </div>
-            <div class="td">
+            <div class="td hidden-lg">
               <div class="double-value-holder">
                 <span class="double-value">-1.26%</span>
                 <span class="double-value">5.21%</span>
               </div>
             </div>
-          </a>
-          <a href="#" class="tr">
-            <div class="td">Cash</div>
-            <div class="td"><span class="value">15 957 949</span></div>
-            <div class="td">(200 889)</div>
-            <div class="td">(200 889)</div>
-            <div class="td">
+          </div>
+          <div class="tr">
+            <div class="td">Real Estate</div>
+            <div class="td"><a href="#" class="value">15 957 949</a></div>
+            <div class="td hidden-sm">(200 889)</div>
+            <div class="td hidden-sm">(200 889)</div>
+            <div class="td hidden-lg">
               <div class="double-value-holder">
                 <span class="double-value">-1.26%</span>
                 <span class="double-value">5.21%</span>
               </div>
             </div>
-            <div class="td">
+            <div class="td hidden-lg">
               <div class="double-value-holder">
                 <span class="double-value">-1.26%</span>
                 <span class="double-value">5.21%</span>
               </div>
             </div>
-          </a>
-          <a href="#" class="tr">
-            <div class="td">Cash</div>
-            <div class="td"><span class="value">15 957 949</span></div>
-            <div class="td">(200 889)</div>
-            <div class="td">(200 889)</div>
-            <div class="td">
+          </div>
+          <div class="tr">
+            <div class="td">Total </div>
+            <div class="td"><a href="#" class="value">15 957 949</a></div>
+            <div class="td hidden-sm">(200 889)</div>
+            <div class="td hidden-sm">(200 889)</div>
+            <div class="td hidden-lg">
               <div class="double-value-holder">
                 <span class="double-value">-1.26%</span>
                 <span class="double-value">5.21%</span>
               </div>
             </div>
-            <div class="td">
+            <div class="td hidden-lg">
               <div class="double-value-holder">
                 <span class="double-value">-1.26%</span>
                 <span class="double-value">5.21%</span>
               </div>
             </div>
-          </a>
-          <a href="#" class="tr">
-            <div class="td">Cash</div>
-            <div class="td"><span class="value">15 957 949</span></div>
-            <div class="td">(200 889)</div>
-            <div class="td">(200 889)</div>
-            <div class="td">
-              <div class="double-value-holder">
-                <span class="double-value">-1.26%</span>
-                <span class="double-value">5.21%</span>
-              </div>
-            </div>
-            <div class="td">
-              <div class="double-value-holder">
-                <span class="double-value">-1.26%</span>
-                <span class="double-value">5.21%</span>
-              </div>
-            </div>
-          </a>
-          <a href="#" class="tr">
-            <div class="td">Cash</div>
-            <div class="td"><span class="value">15 957 949</span></div>
-            <div class="td">(200 889)</div>
-            <div class="td">(200 889)</div>
-            <div class="td">
-              <div class="double-value-holder">
-                <span class="double-value">-1.26%</span>
-                <span class="double-value">5.21%</span>
-              </div>
-            </div>
-            <div class="td">
-              <div class="double-value-holder">
-                <span class="double-value">-1.26%</span>
-                <span class="double-value">5.21%</span>
-              </div>
-            </div>
-          </a>
-          <a href="#" class="tr">
-            <div class="td">Cash</div>
-            <div class="td"><span class="value">15 957 949</span></div>
-            <div class="td">(200 889)</div>
-            <div class="td">(200 889)</div>
-            <div class="td">
-              <div class="double-value-holder">
-                <span class="double-value">-1.26%</span>
-                <span class="double-value">5.21%</span>
-              </div>
-            </div>
-            <div class="td">
-              <div class="double-value-holder">
-                <span class="double-value">-1.26%</span>
-                <span class="double-value">5.21%</span>
-              </div>
-            </div>
-          </a>
-          <a href="#" class="tr">
-            <div class="td">Cash</div>
-            <div class="td"><span class="value">15 957 949</span></div>
-            <div class="td">(200 889)</div>
-            <div class="td">(200 889)</div>
-            <div class="td">
-              <div class="double-value-holder">
-                <span class="double-value">-1.26%</span>
-                <span class="double-value">5.21%</span>
-              </div>
-            </div>
-            <div class="td">
-              <div class="double-value-holder">
-                <span class="double-value">-1.26%</span>
-                <span class="double-value">5.21%</span>
-              </div>
-            </div>
-          </a>
-          <a href="#" class="tr">
-            <div class="td">Cash</div>
-            <div class="td"><span class="value">15 957 949</span></div>
-            <div class="td">(200 889)</div>
-            <div class="td">(200 889)</div>
-            <div class="td">
-              <div class="double-value-holder">
-                <span class="double-value">-1.26%</span>
-                <span class="double-value">5.21%</span>
-              </div>
-            </div>
-            <div class="td">
-              <div class="double-value-holder">
-                <span class="double-value">-1.26%</span>
-                <span class="double-value">5.21%</span>
-              </div>
-            </div>
-          </a>
+          </div>
         </div>
       </div>
     </div>
