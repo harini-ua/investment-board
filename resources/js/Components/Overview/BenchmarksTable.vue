73b09047f830f89ff2d33b3a83fad3886b3f4d80
Sergey Harini <harini.ua@gmail.com>
2021-03-02 19:00:44 +0200

    FE-T2: Overview page / add benchmark  table

--- resources/js/Components/Overview/BenchmarksTable.vue
+++ resources/js/Components/Overview/BenchmarksTable.vue
@@ -1,6 +1,61 @@
 <template>
-  <div>
-    <h3>Benchmarks</h3>
+  <div class="block-table">
+    <div class="table-holder">
+      <h3 class="table-title">Benchmarks</h3>
+      <div class="table table--benchmarks">
+        <div class="thead">
+          <div class="tr">
+            <div class="td">Asset name</div>
+            <div class="td">% MTD</div>
+            <div class="td">% YTD</div>
+          </div>
+        </div>
+        <div class="tbody">
+          <div class="tr-holder">
+            <div class="tr">
+              <div class="td">
+                <div class="tooltip">
+                  <svg class="icon icon-info"><use xlink:href="#icon-info"></use></svg>
+                  <div class="tooltip__content">
+                    Perfomance of KFP dynamic asset allocation
+                  </div>
+                </div>Cash
+              </div>
+              <div class="td">15 957 949</div>
+              <div class="td">200 889</div>
+            </div>
+          </div>
+          <div class="tr-holder">
+            <div class="tr">
+              <div class="td">
+                <div class="tooltip">
+                  <svg class="icon icon-info"><use xlink:href="#icon-info"></use></svg>
+                  <div class="tooltip__content">
+                    Perfomance of KFP dynamic asset allocation
+                  </div>
+                </div>Cash
+              </div>
+              <div class="td">15 957 949</div>
+              <div class="td">200 889</div>
+            </div>
+          </div>
+          <div class="tr-holder">
+            <div class="tr">
+              <div class="td">
+                <div class="tooltip">
+                  <svg class="icon icon-info"><use xlink:href="#icon-info"></use></svg>
+                  <div class="tooltip__content">
+                    Perfomance of KFP dynamic asset allocation
+                  </div>
+                </div>Cash
+              </div>
+              <div class="td">15 957 949</div>
+              <div class="td">200 889</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
