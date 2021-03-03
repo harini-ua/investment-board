e3ee15d0061b4b26494794a76ac25f3488aa6959
Sergey Harini <harini.ua@gmail.com>
2021-03-03 19:47:54 +0200

    FE-T2: Overview page / fixes

--- resources/js/Shared/FiltersWrapper.vue
+++ resources/js/Shared/FiltersWrapper.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="welcome-block">
-    <h1>Welcome, {{ $page.auth.user.first_name }} {{ $page.auth.user.last_name }}</h1>
-    <p>Please pick the date range, base currency, asset class, custodians and account if you need a custom consolidated summary.</p>
+    <div class="welcome-block__content">
+      <h1>Welcome, {{ $page.auth.user.first_name }} {{ $page.auth.user.last_name }}</h1>
+      <p>Please pick the date range, base currency, asset class, custodians and account if you need a custom consolidated summary.</p>
+    </div>
     <div class="welcome-block__row-actions">
       <slot />
     </div>
