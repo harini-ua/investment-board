1b447b675eb282d72ce2f2c267fc3876d82fd44d
Sergey Harini <harini.ua@gmail.com>
2021-03-10 13:50:09 +0200

    Responsive chart for overview page

---
+++ resources/js/Components/Overview/PortfolioAllocationChart.vue
@@ -0,0 +1,125 @@
+<template>
+  <div class="block-chart-wrapper">
+    <div class="block-chart">
+      <h2 class="block-chart__title">Portfolio Allocation</h2>
+      <apexchart ref="portfolio-allocation-chart" type="pie" :options="options" :series="series" />
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  metaInfo: { title: 'Portfolio Allocation' },
+  props: {
+    data: Object,
+  },
+  data: function() {
+    return {
+      options: {
+        chart: {
+          id: 'portfolio-allocation-chart',
+        },
+        labels: this.chartLabels(),
+        dataLabels: {
+          enabled: false,
+        },
+        legend: {
+          position: 'right',
+          horizontalAlign: 'left',
+          formatter: function(val, opts) {
+            return val + ' (' + opts.w.globals.series[opts.seriesIndex]+'%)'
+          },
+          offsetY: -25,
+          markers: {
+            width:  16,
+            height: 16,
+          },
+          itemMargin: {
+            vertical: 5,
+          },
+          fontSize: 14,
+          fontFamily: 'Inter',
+          fontWeight: 500,
+          labels: {
+            colors: '#3E4756',
+          },
+        },
+        responsive: [
+          {
+            breakpoint: 1023,
+            options: {
+              legend: {
+                position: 'bottom',
+                fontSize: 14,
+              },
+            },
+          },{
+            breakpoint: 767,
+            options: {
+              legend: {
+                offsetX: -30,
+                offsetY: 0,
+                position: 'bottom',
+                fontSize: 12,
+              },
+            },
+          },{
+            breakpoint: 424,
+            options: {
+              legend: {
+                offsetX: -30,
+                offsetY: 0,
+                position: 'bottom',
+                fontSize: 12,
+              },
+            },
+          },{
+            breakpoint: 374,
+            options: {
+              legend: {
+                offsetX: -30,
+                offsetY: 0,
+                position: 'bottom',
+                fontSize: 12,
+              },
+            },
+          },{
+            breakpoint: 319,
+            options: {
+              legend: {
+                offsetX: -30,
+                offsetY: 0,
+                position: 'bottom',
+                fontSize: 12,
+              },
+            },
+          },
+        ],
+      },
+      series: this.chartSeries(),
+    }
+  },
+  computed: {
+    //
+  },
+  watch: {
+    data() {
+      this.options = {
+        labels: [...Object.keys(this.data)],
+      }
+      this.series = {
+        data: [...Object.keys(this.data)],
+      }
+    },
+  },
+  methods : {
+    chartLabels() {
+      return [...Object.keys(this.data)]
+    },
+    chartSeries() {
+      return [...Object.values(this.data)]
+    },
+  },
+}
+</script>
---
+++ resources/js/Components/Overview/TotalWealthAllocationChart.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="block-chart-wrapper">
+    <div class="block-chart">
+      <h2 class="block-chart__title">Total Wealth Allocation</h2>
+      <apexchart ref="total-wealth-allocation-chart" type="pie" :options="options" :series="series" />
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  metaInfo: { title: 'Total Wealth Allocation' },
+  props: {
+    data: Object,
+  },
+  data: function() {
+    return {
+      options: {
+        chart: {
+          id: 'total-wealth-allocation-chart',
+        },
+        labels: ['Portfolio Assets', 'Category A', 'Category B'],
+        dataLabels: {
+          enabled: false,
+        },
+        legend: {
+          position: 'right',
+          horizontalAlign: 'left',
+          formatter: function(val, opts) {
+            return val + ' (' + opts.w.globals.series[opts.seriesIndex]+'%)'
+          },
+          offsetY: -25,
+          markers: {
+            width:  16,
+            height: 16,
+          },
+          itemMargin: {
+            vertical: 5,
+            horizontal: 10,
+          },
+          fontSize: 14,
+          fontFamily: 'Inter',
+          fontWeight: 500,
+          labels: {
+            colors: '#3E4756',
+          },
+        },
+        responsive: [
+          {
+            breakpoint: 1023,
+            options: {
+              legend: {
+                position: 'bottom',
+                fontSize: 14,
+              },
+            },
+          },{
+            breakpoint: 767,
+            options: {
+              legend: {
+                offsetX: -30,
+                offsetY: 0,
+                position: 'bottom',
+                fontSize: 12,
+              },
+            },
+          },{
+            breakpoint: 424,
+            options: {
+              legend: {
+                offsetX: -30,
+                offsetY: 0,
+                position: 'bottom',
+                fontSize: 12,
+              },
+            },
+          },{
+            breakpoint: 374,
+            options: {
+              legend: {
+                offsetX: -30,
+                offsetY: 0,
+                position: 'bottom',
+                fontSize: 12,
+              },
+            },
+          },{
+            breakpoint: 319,
+            options: {
+              legend: {
+                offsetX: -30,
+                offsetY: 0,
+                position: 'bottom',
+                fontSize: 12,
+              },
+            },
+          },
+        ],
+      },
+      series: [34.9, 34.1, 31],
+    }
+  },
+  computed: {
+    //
+  },
+  watch: {
+    //
+  },
+  methods : {
+    //
+  },
+}
+</script>
