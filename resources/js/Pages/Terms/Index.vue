3d1bb253c0cd0b702357f0aa840ed2c798f4883e
Sergey Harini <harini.ua@gmail.com>
2021-03-10 16:45:07 +0200

    FE-T7: Other (Static) pages

--- resources/js/Pages/Terms/Index.vue
+++ resources/js/Pages/Terms/Index.vue
@@ -1,6 +1,48 @@
 <template>
-  <div class="container">
-    <h2>Terms & conditions</h2>
+  <div class="wysiwyg">
+    <div class="container">
+      <h1><!--    <h2>Terms & conditions</h2>--></h1>
+      <h2>Privacy policy</h2>
+      <h3>Privacy policy</h3>
+      <h4>Privacy policy</h4>
+      <h5>Privacy policy</h5>
+      <h6>Privacy policy</h6>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem ea, eveniet ipsa libero nihil ut voluptas voluptatibus. Aliquid dignissimos esse eveniet iure magni molestiae molestias nihil non unde voluptate. Quaerat.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem ea, eveniet ipsa libero nihil ut voluptas voluptatibus. Aliquid dignissimos esse eveniet iure magni molestiae molestias nihil non unde voluptate. Quaerat.</p>
+      <p>Lorem <a href="#">ipsum dolor sit amet</a> ipsum dolor sit amet, consectetur adipisicing elit. Dolorem ea, eveniet ipsa libero nihil ut voluptas voluptatibus. Aliquid dignissimos esse eveniet iure magni molestiae molestias nihil non unde voluptate. Quaerat.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem ea, eveniet ipsa libero nihil ut voluptas voluptatibus. Aliquid dignissimos esse eveniet iure magni molestiae molestias nihil non unde voluptate. Quaerat.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem ea, eveniet ipsa libero nihil ut voluptas voluptatibus. Aliquid dignissimos esse eveniet iure magni molestiae molestias nihil non unde voluptate. Quaerat.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem ea, <strong>eveniet ipsa libero nihil</strong> eveniet ipsa libero nihil ut voluptas voluptatibus. Aliquid dignissimos esse eveniet iure magni molestiae molestias nihil non unde voluptate. Quaerat.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem ea, eveniet ipsa libero nihil ut voluptas voluptatibus. Aliquid dignissimos esse eveniet iure magni molestiae molestias nihil non unde voluptate. Quaerat.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur <a href="#">ipsum dolor sit amet</a> adipisicing elit. Dolorem ea, eveniet ipsa libero nihil ut voluptas voluptatibus. Aliquid dignissimos esse eveniet iure magni molestiae molestias nihil non unde voluptate. Quaerat.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem ea, eveniet ipsa libero nihil ut voluptas voluptatibus. Aliquid dignissimos esse eveniet iure magni molestiae molestias nihil non unde voluptate. Quaerat.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem ea, eveniet ipsa libero nihil ut voluptas voluptatibus. Aliquid dignissimos esse eveniet iure magni molestiae molestias nihil non unde voluptate. Quaerat.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem ea, eveniet ipsa libero nihil ut voluptas voluptatibus. Aliquid dignissimos esse eveniet iure magni molestiae molestias nihil non unde voluptate. Quaerat.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem ea, eveniet ipsa libero nihil ut voluptas voluptatibus. Aliquid dignissimos esse eveniet iure magni molestiae molestias nihil non unde voluptate. Quaerat.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem ea, eveniet ipsa libero nihil ut voluptas voluptatibus. Aliquid dignissimos esse eveniet iure magni molestiae molestias nihil non unde voluptate. Quaerat.</p>
+      <ul>
+        <li>Lorem ipsum dolor</li>
+        <li>Lorem ipsum dolor</li>
+        <li>Lorem ipsum dolor</li>
+        <li>Lorem ipsum dolor</li>
+        <li>Lorem ipsum dolor</li>
+        <li>Lorem ipsum dolor</li>
+      </ul>
+      <ol>
+        <li>Lorem ipsum dolor</li>
+        <li>Lorem ipsum dolor</li>
+        <li>Lorem ipsum dolor</li>
+        <li>Lorem ipsum dolor</li>
+        <li>Lorem ipsum dolor</li>
+        <li>Lorem ipsum dolor</li>
+        <li>Lorem ipsum dolor</li>
+        <li>Lorem ipsum dolor</li>
+        <li>Lorem ipsum dolor</li>
+        <li>Lorem ipsum dolor</li>
+        <li>Lorem ipsum dolor</li>
+        <li>Lorem ipsum dolor</li>
+      </ol>
+    </div>
   </div>
 </template>
 
