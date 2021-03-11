6bf00fcf5407a0bbe689decea33b0b1b68fbcd32
Sergey Harini <harini.ua@gmail.com>
2021-03-11 14:01:02 +0200

    FE-T4: Currency page / Currency Exposure table

--- resources/js/Components/Currency/ExposureTable.vue
+++ resources/js/Components/Currency/ExposureTable.vue
@@ -1,5 +1,5 @@
  <template>
-   <div class="block-table">
+   <div class="block-table block-table--currency-exposure">
      <div class="table-holder">
        <h3 class="table-title table-title--currency-exposure">Currency Exposure</h3>
        <div class="table table--currency-exposure">
@@ -13,6 +13,237 @@
            </div>
          </div>
          <div class="tbody">
+           <div :class="[{active: isActive}, 'tr-holder']" @click="openHiddenContent()">
+             <div class="tr">
+               <div class="td">EUR</div>
+               <div class="td">4 738 496</div>
+               <div class="td hidden-sm">36 473 412</div>
+               <div class="td hidden-lg">7 556 976</div>
+               <div class="td hidden-lg">48 768 884</div>
+             </div>
+             <div class="collapse-content collapse-content--general">
+               <div class="collapse-content-table">
+                 <div class="cell-row visible-lg">
+                   <div class="cell-thead">Net Assets</div>
+                   <div class="cell-description">55,6%</div>
+                 </div>
+                 <div class="cell-row visible-md">
+                   <div class="cell-thead">FX Forwards</div>
+                   <div class="cell-description">8,6%</div>
+                 </div>
+               </div>
+             </div>
+           </div>
+           <div :class="[{active: isActive}, 'tr-holder']" @click="openHiddenContent()">
+             <div class="tr">
+               <div class="td">EUR</div>
+               <div class="td">4 738 496</div>
+               <div class="td hidden-sm">36 473 412</div>
+               <div class="td hidden-lg">7 556 976</div>
+               <div class="td hidden-lg">48 768 884</div>
+             </div>
+             <div class="collapse-content collapse-content--general">
+               <div class="collapse-content-table">
+                 <div class="cell-row visible-lg">
+                   <div class="cell-thead">Net Assets</div>
+                   <div class="cell-description">55,6%</div>
+                 </div>
+                 <div class="cell-row visible-md">
+                   <div class="cell-thead">FX Forwards</div>
+                   <div class="cell-description">8,6%</div>
+                 </div>
+               </div>
+             </div>
+           </div>
+           <div :class="[{active: isActive}, 'tr-holder']" @click="openHiddenContent()">
+             <div class="tr">
+               <div class="td">EUR</div>
+               <div class="td">4 738 496</div>
+               <div class="td hidden-sm">36 473 412</div>
+               <div class="td hidden-lg">7 556 976</div>
+               <div class="td hidden-lg">48 768 884</div>
+             </div>
+             <div class="collapse-content collapse-content--general">
+               <div class="collapse-content-table">
+                 <div class="cell-row visible-lg">
+                   <div class="cell-thead">Net Assets</div>
+                   <div class="cell-description">55,6%</div>
+                 </div>
+                 <div class="cell-row visible-md">
+                   <div class="cell-thead">FX Forwards</div>
+                   <div class="cell-description">8,6%</div>
+                 </div>
+               </div>
+             </div>
+           </div>
+           <div :class="[{active: isActive}, 'tr-holder']" @click="openHiddenContent()">
+             <div class="tr">
+               <div class="td">EUR</div>
+               <div class="td">4 738 496</div>
+               <div class="td hidden-sm">36 473 412</div>
+               <div class="td hidden-lg">7 556 976</div>
+               <div class="td hidden-lg">48 768 884</div>
+             </div>
+             <div class="collapse-content collapse-content--general">
+               <div class="collapse-content-table">
+                 <div class="cell-row visible-lg">
+                   <div class="cell-thead">Net Assets</div>
+                   <div class="cell-description">55,6%</div>
+                 </div>
+                 <div class="cell-row visible-md">
+                   <div class="cell-thead">FX Forwards</div>
+                   <div class="cell-description">8,6%</div>
+                 </div>
+               </div>
+             </div>
+           </div>
+           <div :class="[{active: isActive}, 'tr-holder']" @click="openHiddenContent()">
+             <div class="tr">
+               <div class="td">EUR</div>
+               <div class="td">4 738 496</div>
+               <div class="td hidden-sm">36 473 412</div>
+               <div class="td hidden-lg">7 556 976</div>
+               <div class="td hidden-lg">48 768 884</div>
+             </div>
+             <div class="collapse-content collapse-content--general">
+               <div class="collapse-content-table">
+                 <div class="cell-row visible-lg">
+                   <div class="cell-thead">Net Assets</div>
+                   <div class="cell-description">55,6%</div>
+                 </div>
+                 <div class="cell-row visible-md">
+                   <div class="cell-thead">FX Forwards</div>
+                   <div class="cell-description">8,6%</div>
+                 </div>
+               </div>
+             </div>
+           </div>
+           <div :class="[{active: isActive}, 'tr-holder']" @click="openHiddenContent()">
+             <div class="tr">
+               <div class="td">EUR</div>
+               <div class="td">4 738 496</div>
+               <div class="td hidden-sm">36 473 412</div>
+               <div class="td hidden-lg">7 556 976</div>
+               <div class="td hidden-lg">48 768 884</div>
+             </div>
+             <div class="collapse-content collapse-content--general">
+               <div class="collapse-content-table">
+                 <div class="cell-row visible-lg">
+                   <div class="cell-thead">Net Assets</div>
+                   <div class="cell-description">55,6%</div>
+                 </div>
+                 <div class="cell-row visible-md">
+                   <div class="cell-thead">FX Forwards</div>
+                   <div class="cell-description">8,6%</div>
+                 </div>
+               </div>
+             </div>
+           </div>
+           <div :class="[{active: isActive}, 'tr-holder']" @click="openHiddenContent()">
+             <div class="tr">
+               <div class="td">EUR</div>
+               <div class="td">4 738 496</div>
+               <div class="td hidden-sm">36 473 412</div>
+               <div class="td hidden-lg">7 556 976</div>
+               <div class="td hidden-lg">48 768 884</div>
+             </div>
+             <div class="collapse-content collapse-content--general">
+               <div class="collapse-content-table">
+                 <div class="cell-row visible-lg">
+                   <div class="cell-thead">Net Assets</div>
+                   <div class="cell-description">55,6%</div>
+                 </div>
+                 <div class="cell-row visible-md">
+                   <div class="cell-thead">FX Forwards</div>
+                   <div class="cell-description">8,6%</div>
+                 </div>
+               </div>
+             </div>
+           </div>
+           <div :class="[{active: isActive}, 'tr-holder']" @click="openHiddenContent()">
+             <div class="tr">
+               <div class="td">EUR</div>
+               <div class="td">4 738 496</div>
+               <div class="td hidden-sm">36 473 412</div>
+               <div class="td hidden-lg">7 556 976</div>
+               <div class="td hidden-lg">48 768 884</div>
+             </div>
+             <div class="collapse-content collapse-content--general">
+               <div class="collapse-content-table">
+                 <div class="cell-row visible-lg">
+                   <div class="cell-thead">Net Assets</div>
+                   <div class="cell-description">55,6%</div>
+                 </div>
+                 <div class="cell-row visible-md">
+                   <div class="cell-thead">FX Forwards</div>
+                   <div class="cell-description">8,6%</div>
+                 </div>
+               </div>
+             </div>
+           </div>
+           <div :class="[{active: isActive}, 'tr-holder']" @click="openHiddenContent()">
+             <div class="tr">
+               <div class="td">EUR</div>
+               <div class="td">4 738 496</div>
+               <div class="td hidden-sm">36 473 412</div>
+               <div class="td hidden-lg">7 556 976</div>
+               <div class="td hidden-lg">48 768 884</div>
+             </div>
+             <div class="collapse-content collapse-content--general">
+               <div class="collapse-content-table">
+                 <div class="cell-row visible-lg">
+                   <div class="cell-thead">Net Assets</div>
+                   <div class="cell-description">55,6%</div>
+                 </div>
+                 <div class="cell-row visible-md">
+                   <div class="cell-thead">FX Forwards</div>
+                   <div class="cell-description">8,6%</div>
+                 </div>
+               </div>
+             </div>
+           </div>
+           <div :class="[{active: isActive}, 'tr-holder']" @click="openHiddenContent()">
+             <div class="tr">
+               <div class="td">EUR</div>
+               <div class="td">4 738 496</div>
+               <div class="td hidden-sm">36 473 412</div>
+               <div class="td hidden-lg">7 556 976</div>
+               <div class="td hidden-lg">48 768 884</div>
+             </div>
+             <div class="collapse-content collapse-content--general">
+               <div class="collapse-content-table">
+                 <div class="cell-row visible-lg">
+                   <div class="cell-thead">Net Assets</div>
+                   <div class="cell-description">55,6%</div>
+                 </div>
+                 <div class="cell-row visible-md">
+                   <div class="cell-thead">FX Forwards</div>
+                   <div class="cell-description">8,6%</div>
+                 </div>
+               </div>
+             </div>
+           </div>
+           <div :class="[{active: isActive}, 'tr-holder']" @click="openHiddenContent()">
+             <div class="tr">
+               <div class="td">EUR</div>
+               <div class="td">4 738 496</div>
+               <div class="td hidden-sm">36 473 412</div>
+               <div class="td hidden-lg">7 556 976</div>
+               <div class="td hidden-lg">48 768 884</div>
+             </div>
+             <div class="collapse-content collapse-content--general">
+               <div class="collapse-content-table">
+                 <div class="cell-row visible-lg">
+                   <div class="cell-thead">Net Assets</div>
+                   <div class="cell-description">55,6%</div>
+                 </div>
+                 <div class="cell-row visible-md">
+                   <div class="cell-thead">FX Forwards</div>
+                   <div class="cell-description">8,6%</div>
+                 </div>
+               </div>
+             </div>
+           </div>
            <div :class="[{active: isActive}, 'tr-holder']" @click="openHiddenContent()">
              <div class="tr">
                <div class="td">EUR</div>
