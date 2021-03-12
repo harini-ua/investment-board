f189cf14418fd233962db7331a5637c82669ae10
Sergey Harini <harini.ua@gmail.com>
2021-03-12 15:41:15 +0200

    Update profile menu component

--- resources/js/Shared/ProfileMenu.vue
+++ resources/js/Shared/ProfileMenu.vue
@@ -1,22 +1,18 @@
 <template>
   <div v-click-outside="closeProfileMenu">
-    <button class="user-account">
-<!--      <div class="user-name"></div>-->
-      <div class="icon-user" @click.prevent="show=!show">
+    <button :class="[{ active : show }, 'user-account']" @click.prevent="show=!show">
+      <div class="icon-user">
         <img src="../../images/icon-user.png" alt="">
       </div>
-<!--      Arrow icon-->
+      <svg class="icon icon-arrow-down"><use xlink:href="#icon-arrow-down"></use></svg>
       <div v-if="show" class="logout-dropdown" @click.prevent="show=!show">
-
-            <inertia-link :href="route('logout')" method="post"><svg class="icon icon-log-out"><use xlink:href="#icon-log-out"></use></svg> Logout</inertia-link>
-
+        <inertia-link :href="route('logout')" method="post"><svg class="icon icon-log-out"><use xlink:href="#icon-log-out"></use></svg> Logout</inertia-link>
       </div>
     </button>
   </div>
 </template>
 
 <script>
-
 export default {
   data() {
     return {
