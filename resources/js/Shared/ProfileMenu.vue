f41ce20f9175a3be3fac13c1282e513b31dc6b10
Sergey Harini <harini.ua@gmail.com>
2021-04-12 17:45:47 +0300

    Update style drop down

--- resources/js/Shared/ProfileMenu.vue
+++ resources/js/Shared/ProfileMenu.vue
@@ -6,7 +6,7 @@
       </div>
       <svg class="icon icon-arrow-down"><use xlink:href="#icon-arrow-down"></use></svg>
       <div v-if="show" class="logout-dropdown" @click.prevent="show=!show">
-        <ul>
+        <ul class="logout-dropdown__list">
           <li>Currency: {{ userCurrency }}</li>
           <li>
             <inertia-link :href="route('logout')" method="post"><svg class="icon icon-log-out"><use xlink:href="#icon-log-out"></use></svg> Logout</inertia-link>
